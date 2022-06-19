88e5a64447abcac8de523f3fa46d67f7e07e8e28
Hugo Herrador Carrasco <hugo.herrador@geograma.com>
2022-06-19 12:09:45 +0200

    MyButton class now exposes the props of the IMyButtonProps interface as externally aavailable for setting and internally available for use

--- src/MyButton.tsx
+++ src/MyButton.tsx
@@ -1,10 +1,17 @@
 import { Button } from "@material-ui/core";
 import React from "react";
 
-export class MyButton extends React.Component {
+export interface IMyButtonProps {
+    buttonName: string;
+    handleButtonClick(): void;
+}
+export class MyButton extends React.Component<IMyButtonProps> {
     render () {
         return (
-            <Button color="primary">Click me</Button>
+            <Button color="primary"
+                onClick={this.onButtonClicked}>
+                {this.props.buttonName}
+            </Button>
         )
     }
 }
