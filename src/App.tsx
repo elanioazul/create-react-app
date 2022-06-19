d93834a00c8d1ad490d50b40355f8d7fb33fbf07
Hugo Herrador Carrasco <hugo.herrador@geograma.com>
2022-06-19 16:19:35 +0200

    Add a minimal expresion of what a react component state might be. Interface included in the generic component difinition plus set initial value of the state property

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,35 @@ import './App.css';
 import { MyButton } from './MyButton';
 
 export interface IAppProps {};
-class App extends React.Component<IAppProps> {
+export interface IAppState {
+  showDetails: boolean;
+}
+class App extends React.Component<IAppProps, IAppState> {
   constructor(props: IAppProps) {
     super(props);
-    this.handleClick = this.handleClick.bind(this)
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      showDetails: false
+    }
   }
   render() {
     return (
-      <MyButton
-        buttonName='Click here'
-        handleButtonClick={this.handleClick}
-      ></MyButton>
+      <div>
+        <p>showDetails = {this.state.showDetails ? "true" : "false"}
+        </p>
+        <MyButton
+          buttonName='Click here'
+          handleButtonClick={this.handleClick}
+        ></MyButton>
+      </div>
+
     )
   }
   handleClick() {
-    console.log(`App handling the click , youh!`)
+    console.log(`App handling the click , youh!`);
+    this.setState({
+      showDetails: !this.state.showDetails
+    })
   }
 }
 
