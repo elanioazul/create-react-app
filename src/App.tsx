1212cd32988ebae4bb88d0779f84cb2bd2751052
Hugo Herrador Carrasco <hugo.herrador@geograma.com>
2022-06-19 11:34:40 +0200

    Add logic out of the return statement to determines what to render. Thus. the resulting JSX is a variable storing html

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,21 @@ import './App.css';
 
 class App extends React.Component {
   render() {
+    let item = null;
+    if (Math.random() < 0.5) {
+      item = (
+        <p>Random less than 0.5</p>
+      )
+    } else {
+      item = (
+        <p>Random superior than 0.5</p>
+      )
+    }
     return (
-      <div>Hello JSX</div>
+      <div>
+        {item}
+      </div>
     )
-      
   }
 }
 
