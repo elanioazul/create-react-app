77ff8e5aec17c93aa2ad6ebfb46d82bd56209ef7
Hugo Herrador Carrasco <hugo.herrador@geograma.com>
2022-06-19 11:55:30 +0200

    Install @material-ui core and icons and create MyButton component used in class App

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
 import React from 'react';
 import './App.css';
-
+import { MyButton } from './MyButton';
 class App extends React.Component {
   render() {
-    let item = null;
-    if (Math.random() < 0.5) {
-      item = (
-        <p>Random less than 0.5</p>
-      )
-    } else {
-      item = (
-        <p>Random superior than 0.5</p>
-      )
-    }
     return (
-      <div>
-        {item}
-      </div>
+      <MyButton></MyButton>
     )
   }
 }
---
+++ src/MyButton.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@material-ui/core";
+import React from "react";
+
+export class MyButton extends React.Component {
+    render () {
+        return (
+            <Button color="primary">Click me</Button>
+        )
+    }
+}
